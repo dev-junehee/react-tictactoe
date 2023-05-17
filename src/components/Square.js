ed41faee530cb9abc48e5f750a9500e81e252766
junehee <dev.junehee@gmail.com>
2023-05-17 15:03:03 +0900

    react : tic-tac-toe

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import './Square.css'
+
+export default function Square(props) {
+  
+  return (
+    <button className="square"
+      onClick={() => {props.onClick()}}>
+      {props.value}
+    </button>
+  )
+}
